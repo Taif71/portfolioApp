79e490f5ad9a2828d77d0d00b52f9f6ad0f387b6
Taif <yodabdeducation@gmail.com>
2020-04-19 20:50:45 +0600

    Updates

---
+++ precache-manifest.e9efc72858fd420dad915d1239c1d60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9641339ef701efdb203e25c3ae410410",
+    "url": "/portfolioApp/index.html"
+  },
+  {
+    "revision": "b1a15aec7022b0175e8c",
+    "url": "/portfolioApp/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ef6fb9a54f15d3e87851",
+    "url": "/portfolioApp/static/js/2.ce8a6c0c.chunk.js"
+  },
+  {
+    "revision": "3f007170c883e3473717adb3ff184152",
+    "url": "/portfolioApp/static/js/2.ce8a6c0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1a15aec7022b0175e8c",
+    "url": "/portfolioApp/static/js/main.389130b2.chunk.js"
+  },
+  {
+    "revision": "e62a1070dae0240a1809",
+    "url": "/portfolioApp/static/js/runtime-main.e84a2c9c.js"
+  }
+]);
